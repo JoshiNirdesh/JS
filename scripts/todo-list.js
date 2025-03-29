20b84e43a8c3b3907066dcc6d66b450fdaba141a
JoshiNirdesh <joshinirdesh10@gmail.com>
2025-03-29 17:20:37 +0545

    advanced function

--- scripts/todo-list.js
+++ scripts/todo-list.js
@@ -1,12 +1,28 @@
-const todoList=[];
+const todoList=[{
+    name : 'make dinner',
+    dueDate: '2022-12-22'
+},{
+    name: 'fs',
+    dueDate:'2022-12-4'
+}];
 
 render();
 function render (){
 let todoListHTML = '';
 
 for(let i =0;i<todoList.length;i++){
-    const todo = todoList[i];
-    const html = `<p>${todo}</p>`;
+    const todoObject = todoList[i];
+    const name = todoObject.name;
+    const dueDate = todoObject.dueDate;
+    const html = `
+    <p>
+    ${name} ${dueDate}
+    <button onclick="
+    todoList.splice(${i},1);
+    render();
+    ">Delete</button>
+    </p>
+    `;
     todoListHTML=todoListHTML+html;
     console.log(todoListHTML)
 }
@@ -16,6 +32,7 @@ document.querySelector('.js-todo-list').innerHTML=todoListHTML;
 function addTodo(){
     const inputElement=document.querySelector(".js-input")
     const name = inputElement.value;
+    const dateInputElement =document.querySelector('.js-due-date')
     todoList.push(name); 
     console.log(todoList);
     inputElement.value='';
