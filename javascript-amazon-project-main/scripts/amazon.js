faf0e23d693d1c2df54e5a0f31cb70f8ab6518b1
JoshiNirdesh <joshinirdesh10@gmail.com>
2025-04-11 05:16:57 +0545

    cart dynamic

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,12 +1,41 @@
-import { cart } from "../data/cart.js";
+const products = [
+  {
+    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+    name: " Black and Gray Athletic Cotton Socks - 6 Pairs",
+    rating: {
+      stars: 4.5,
+      count: 87,
+    },
+    price: 1090,
+  },
+  {
+    image: "images/products/intermediate-composite-basketball.jpg",
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4.0,
+      count: 127,
+    },
+    price: 2095,
+  },
+  {
+    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+    name: " Adults Plain Cotton T-Shirt - 2 Pack",
+    rating: {
+      stars: 4.5,
+      count: 54,
+    },
+    price: 799,
+  },
+];
 
 let productsHTML = "";
-
 products.forEach((product) => {
-  productsHTML += ` 
-    <div class="product-container">
+  productsHTML += `<div class="product-container">
           <div class="product-image-container">
-            <img class="product-image" src=${product.image}>
+            <img
+              class="product-image"
+              src="${product.image}"
+            />
           </div>
 
           <div class="product-name limit-text-to-2-lines">
@@ -14,16 +43,16 @@ products.forEach((product) => {
           </div>
 
           <div class="product-rating-container">
-            <img class="product-rating-stars"
-              src="images/ratings/rating-${product.rating.stars * 10}.png">
-            <div class="product-rating-count link-primary">
-              ${product.rating.stars}
-            </div>
+            <img
+              class="product-rating-stars"
+              src="images/ratings/rating-${product.rating.stars * 10}.png"
+            />
+            <div class="product-rating-count link-primary">${
+              product.rating.count
+            }</div>
           </div>
 
-          <div class="product-price">
-            $${product.price / 100};
-          </div>
+          <div class="product-price">$${(product.price / 100).toFixed(2)}</div>
 
           <div class="product-quantity-container">
             <select>
@@ -43,43 +72,11 @@ products.forEach((product) => {
           <div class="product-spacer"></div>
 
           <div class="added-to-cart">
-            <img src="images/icons/checkmark.png">
+            <img src="images/icons/checkmark.png" />
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${
-            product.id
-          }">
-            Add to Cart
-          </button>
+          <button class="add-to-cart-button button-primary">Add to Cart</button>
         </div>`;
 });
 document.querySelector(".js-products-grid").innerHTML = productsHTML;
-
-document.querySelectorAll(".js-add-to-cart").forEach((button) => {
-  button.addEventListener("click", () => {
-    const productId = button.dataset.productId;
-
-    let matchingItem;
-    cart.forEach((item) => {
-      if (productId === item.productId) {
-        matchingItem = item;
-      }
-    });
-    if (matchingItem) {
-      matchingItem.quantity += 1;
-    } else {
-      cart.push({
-        productId: productId,
-        quantity: 1,
-      });
-    }
-    let cartQuantity = 0;
-    cart.forEach((item) => {
-      cartQuantity += item.quantity;
-    });
-    document.querySelector(".js-cart-quantity").innerHTML =
-      console.log(cartQuantity);
-    console.log(cart);
-  });
-});
